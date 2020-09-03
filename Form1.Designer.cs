d9500507f4c575d7ff7a890ba995af6af584cc79
CodeCrazy97 <evaughan72@yahoo.com>
2020-09-03 12:15:48 -0400

    First commit. Created basic GUI

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace VASCOR_Exercise
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.documentFinderButton = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // documentFinderButton
+            // 
+            this.documentFinderButton.Location = new System.Drawing.Point(51, 13);
+            this.documentFinderButton.Name = "documentFinderButton";
+            this.documentFinderButton.Size = new System.Drawing.Size(190, 45);
+            this.documentFinderButton.TabIndex = 0;
+            this.documentFinderButton.Text = "Find 928 Document";
+            this.documentFinderButton.UseVisualStyleBackColor = true;
+            this.documentFinderButton.Click += new System.EventHandler(this.documentFinderButton_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(51, 80);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(632, 225);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.documentFinderButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button documentFinderButton;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VASCOR_Exercise
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+
+        private void documentFinderButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.ShowDialog();
+            Console.WriteLine("Fname = " + openFileDialog1.FileName);
+            string fileName = openFileDialog1.FileName;
+            try
+            {
+                if (fileName.Substring(fileName.Length - 3).Equals("txt"))
+                {
+                    // Read contents of the 928.
+                    string[] lines = System.IO.File.ReadAllLines(@fileName);
+
+                    foreach (string line in lines)
+                    {
+                        // Use a tab to indent each line of the file.
+                        richTextBox1.AppendText("\t" + line);
+                    }
+                } else
+                {
+                    richTextBox1.Text = "Invalid filetype! Must be a .txt file.";
+                }
+            }
+            catch (ArgumentOutOfRangeException e2)
+            {
+                richTextBox1.Text = "ERROR: " + e2.Message;
+            }
+        }
+    }
+}
